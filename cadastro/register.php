4aa258f3c574189f21e7d3e3284761d304e8d8ab
Lucas Bogos <lucas@pop-os.localdomain>
2022-02-25 22:01:10 -0300

    corrigi estilos da pagina e o html

--- cadastro/register.php
+++ cadastro/register.php
@@ -1,39 +1,55 @@
-<?php
-    $host = getenv("HOST");
-    $username = getenv("USERNAME");
-    $password = getenv("PASSWORD");
-    $db_name = getenv("DATABASE_NAME");
- 
-    $connection = new \MySQLi($host, $username, $password, $db_name);
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <link rel="stylesheet" href="../styles/form_register.css">
+      <link rel="stylesheet" href="../styles/globals.css">
+      <title>Cadastro</title>
+  </head>
+  <body>
+    <?php
+      $host = getenv("HOST");
+      $username = getenv("USERNAME");
+      $password = getenv("PASSWORD");
+      $db_name = getenv("DATABASE_NAME");
+    
+      $connection = new \MySQLi($host, $username, $password, $db_name);
 
-    if($connection->connect_error){
+      if($connection->connect_error){
         die("Desconectado! Erro: " . $connection->connect_error);
-    }else{
-        echo "Banco de dados conectado!\n";
-    }
-     
-    if ($_SERVER['REQUEST_METHOD'] == "POST") {
+      }
+        
+      if ($_SERVER['REQUEST_METHOD'] == "POST") {
         $name =  isset($_POST["name"])
-            ? $_POST["name"] : "Não informado";
+          ? $_POST["name"] : "Não informado";
         $email = isset($_POST["email"])
-            ? $_POST["email"] : "Não informado";
+          ? $_POST["email"] : "Não informado";
         $password = isset($_POST["password"])
-            ? $_POST["password"] : "Não informado";
+          ? $_POST["password"] : "Não informado";
         $checked_password = isset($_POST["check_password"])
-            ? $_POST["check_password"] : "Não informado";
+          ? $_POST["check_password"] : "Não informado";
 
         if ($checked_password === $password) {
-            $sql = "INSERT INTO user (name, email, password) VALUES ('{$name}', '{$email}', '{$password}')";
+          $sql = "INSERT INTO user (name, email, password) VALUES ('{$name}', '{$email}', '{$password}')";
 
-            if ($connection->query($sql)) {
-                echo "New record created successfully\n";
-            } else {
-                echo "Error: " . $sql . "<br>" . $connection->error . "\n";
-            }
-            echo "Olá <strong>$name</strong>, seu é email é $email e seu acesso seria $password<br>\n";
+          if ($connection->query($sql)) {
+              echo '
+                <p>Usuário <strong>'. $name .'</strong><span style="color: green;">
+                cadastrado com sucesso!</span></p>
+              ';
+          } else {
+              echo '
+                <p><span style="color: red;"><strong>Error</strong></span>: '. $sql .'<br>'. 
+                $connection->error .'</p>
+              ';
+          }
         } else {
-            echo "Ocorreu um erro: as senhas estão diferentes!\n";
+            echo '<p style="color: red;"><strong>Ocorreu um erro</strong>: as senhas estão diferentes!</p>';
         }
         $connection->close();
-    }
-?>
+      }
+    ?>
+  </body>
+</html>
--- login.php
+++ login.php
@@ -1,39 +1,53 @@
-<?php
-    $host = getenv("HOST");
-    $username = getenv("USERNAME");
-    $password = getenv("PASSWORD");
-    $db_name = getenv("DATABASE_NAME");
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="../styles/form_register.css">
+  <link rel="stylesheet" href="../styles/globals.css">
+  <title>Login</title>
+</head>
+<body>
+  <?php
+  $host = getenv("HOST");
+  $username = getenv("USERNAME");
+  $password = getenv("PASSWORD");
+  $db_name = getenv("DATABASE_NAME");
 
-    $connection = new \MySQLi($host, $username, $password, $db_name);
+  $connection = new \MySQLi($host, $username, $password, $db_name);
 
-    if($connection->connect_error){
-        die("<p>Banco de dados desconectado! <strong>Erro</strong>: " 
-        . $connection->connect_error . "</p>");
+  if ($connection->connect_error) {
+    die("<p>Banco de dados desconectado! <strong>Erro</strong>: "
+      . $connection->connect_error . "</p>");
+  }
+
+  if ($_SERVER['REQUEST_METHOD'] == "POST") {
+    $email = isset($_POST["email"])
+      ? $_POST["email"] : "Não informado";
+    $password = isset($_POST["password"])
+      ? $_POST["password"] : "Não informado";
+
+    $sql = "SELECT COUNT(*) FROM user WHERE email = '{$email}' and password = '{$password}'";
+    $result = $connection->query($sql);
+    $row = $result->fetch_row();
+
+    if ($row[0] > 0) {
+      echo '
+        <h2>E-mail <span style="color:red;">' . $email . '</span> já está cadastrado</h2>
+        <p>Caso a intensão seja fazer outro cadastro, use 
+        <a href="/cadastro/">página de cadastro</a></p>
+      ';
+    } else {
+      echo '
+        <h2>Usuário inexistente</h2>
+        <p>Realize seu cadastro em nosso site, através do 
+        <a href="/cadastro/">link</a></p>
+      ';
     }
+    $connection->close();
+  }
+  ?>
+</body>
 
-    if ($_SERVER['REQUEST_METHOD'] == "POST") {
-        $email = isset($_POST["email"])
-            ? $_POST["email"] : "Não informado";
-        $password = isset($_POST["password"])
-            ? $_POST["password"] : "Não informado";
-            
-        $sql = "SELECT COUNT(*) FROM user WHERE email = '{$email}' and password = '{$password}'";
-        $result = $connection->query($sql);
-        $row = $result->fetch_row();
-
-        if ($row[0] > 0) {
-            echo '
-                <h2>E-mail <span style="color:red;">'. $email .'</span> já está cadastrado</h2>
-                <p>Caso a intensão seja fazer outro cadastro, use 
-                <a href="/cadastro/">página de cadastro</a></p>
-            ';
-        } else {
-            echo '
-                <h2>Usuário inexistente</h2>
-                <p>Realize seu cadastro em nosso site, através do 
-                <a href="/cadastro/">link</a></p>
-            ';
-        }
-        $connection->close();
-    }
-?>
+</html>
